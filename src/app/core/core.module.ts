32da2c94a83cb25faa7be688abadaa22c8da41f6
TonioX <t_toniox@hotmail.fr>
2020-08-24 16:33:38 +0200

    Install bootstrap + FontAwesome

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { IconsModule } from '../icons/icons.module';
+import { LoginModule } from '../login/login.module';
+import { TemplatesModule } from '../templates/templates.module';
+import { TextModule } from '../text/text.module';
+import { UiModule } from '../ui/ui.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports:[
+    LoginModule,
+    IconsModule,
+    TextModule,
+    TemplatesModule,
+    UiModule
+  ]
+})
+export class CoreModule { }
---
+++ src/app/icons/icons.module.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FontAwesomeModule
+  ]
+})
+export class IconsModule { }
